1571ce23a93be42ab5f16c7418ba78799822e88b
MUHAMMED MUSTAFA COBAN <e2309854@ceng.metu.edu.tr>
2023-02-07 12:49:28 +0100

    init

---
+++ src/main/kotlin/com/savrona/agilepoker/AgilepokerApplication.kt
@@ -0,0 +1,11 @@
+package com.savrona.agilepoker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgilepokerApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgilepokerApplication>(*args)
+}
---
+++ src/test/kotlin/com/savrona/agilepoker/AgilepokerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.savrona.agilepoker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgilepokerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
